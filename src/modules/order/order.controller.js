a73a6d1d7065547e608767500f4c786997f5b576
Mahmoud Mohamed <ma5975986@gmail.com>
2025-07-18 19:06:10 +0300

    Refactor order management: update createOrder to use productModel, add deleteOrderById function, and adjust routes accordingly

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -1,28 +1,29 @@
 import { orderModel } from "../../../db/models/order.model.js";
+import { productModel } from "../../../db/models/product.model.js";
 
 let createOrder = async (req, res) => {
-    const { products, shippingAddress } = req.body;
+    const { user, items, shippingAddress } = req.body;
     try {
-        if (!products || products.length === 0) {
+        if (!items || items.length === 0) {
             return res.status(400).json({ message: "There's no products found." });
         }
         let totalPrice = 0;
-        for (const item of products) {
-            const dbProduct = await orderModel.findById(item.product);
+        for (const item of items) {
+            const dbProduct = await productModel.findById(item.product);
             if (!dbProduct) {
                 return res.status(404).json({ message: "Product is not found" });
             }
-            totalPrice += dbProduct.price * (item.quantity || 1);
+            totalPrice += dbProduct.price * item.quantity;
         }
         const order = await orderModel.insertOne(
             {
-                products: products,
+                user: user,
+                items: items,
                 shippingAddress: shippingAddress,
                 totalPrice: totalPrice,
             }
         );
         res.status(201).json({ message: "Order has been created successfully!", order: order });
-
     } catch (error) {
         res.status(500).json({ message: "Error occured when creating the order.", error });
     }
@@ -30,10 +31,10 @@ let createOrder = async (req, res) => {
 
 let getUserOrders = async (req, res) => {
     try {
-        const orders = await orderModel.find({ user: req.user.id }).populate("products.product");
+        const orders = await orderModel.find({ user: req.body.user }).populate("items");
         res.status(200).json(orders);
     } catch (error) {
-        res.status(500).json({ message: "Error occured when fetching the orders" });
+        res.status(500).json({ message: "Error occured when fetching the orders." });
     }
 };
 
@@ -41,7 +42,7 @@ let getOrderById = async (req, res) => {
     try {
         const order = await orderModel.findById(req.params.id).populate("products.product");
         if (!order) return res.status(404).json({ message: "Order not found." });
-        if (order.user.toString() !== req.user.id)
+        if (order.user != req.user)
             return res.status(403).json({ message: "You're not allowed to access this order." });
         res.status(200).json(order);
     } catch (error) {
@@ -49,8 +50,22 @@ let getOrderById = async (req, res) => {
     }
 };
 
+let deleteOrderById = async (req, res) => {
+    try {
+        let item = await orderModel.findOne({_id: req.params.id});
+        if(!item) return res.status(409).json({message: "Order not found."});
+        if(item.user != req.body.user)
+            return res.status(403).json({message: "You're not allowed to delete this order."}); 
+        await orderModel.findOneAndDelete({_id: req.params.id});
+        res.json({message: "Order deleted successfully!"});
+    } catch (error) {
+        res.status(500).json({message: "Error deleting the order."});
+    }
+};
+
 export {
     createOrder,
     getUserOrders,
     getOrderById,
+    deleteOrderById,
 };
--- src/modules/order/order.routes.js
+++ src/modules/order/order.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createOrder, getOrderById, getUserOrders } from "./order.controller.js";
+import { createOrder, deleteOrderById, getOrderById, getUserOrders } from "./order.controller.js";
 
 export const orderRoutes = express.Router();
 
 orderRoutes.get("/orders", getUserOrders);
-orderRoutes.get("/orders", getOrderById);
+orderRoutes.get("/orders/:id", getOrderById);
 orderRoutes.post("/orders", createOrder);
+orderRoutes.delete("/orders/:id", deleteOrderById);
